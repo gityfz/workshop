085548884a9e84f5d0596aa1a15b4edaa956db9c
ZYF <gityfz@github.com>
2017-10-18 16:09:11 +0800

    firstcommit

---
+++ src/main/java/com/metro/workshop/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.metro.workshop.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping
+public class TestController {
+
+    @RequestMapping
+    @ResponseBody
+    public String test(HttpServletRequest request, HttpServletResponse response){
+        return  "HI,sb";
+    }
+}
