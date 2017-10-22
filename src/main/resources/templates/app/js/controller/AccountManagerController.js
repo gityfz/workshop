d7a47bf17e512a8ef5deea2796c3547a07e805fa
ZYF <gityfz@github.com>
2017-10-22 12:54:43 +0800

    UI

--- src/main/resources/templates/app/js/controller/AccountManagerController.js
+++ src/main/resources/templates/app/js/controller/AccountManagerController.js
@@ -5,13 +5,18 @@ angular.module('MetronicApp').controller('AccountManagerController', function($r
     });
 
  $scope.axiba="axiba";
+$scope.employeename="EEEEEEEM";
 
+    $scope.clickaddbutton=function(){
+        $("#addAccountModal").modal("show");
+    }
     // set sidebar closed and body solid layout mode
     $rootScope.settings.layout.pageContentWhite = true;
     $rootScope.settings.layout.pageBodySolid = false;
     $rootScope.settings.layout.pageSidebarClosed = false;
 
     var locationChangeStartOff = $rootScope.$on('$locationChangeStart', locationChangeStart);
+
 
     function locationChangeStart(event) {
         clearInterval(statsInterval);
